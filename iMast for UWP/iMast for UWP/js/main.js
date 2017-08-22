2a013da8d4e1456271283e2fc3186ba6d84bc50a
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2017-08-23 02:14:48 +0900

    Add sample SplitView.

--- iMast for UWP/iMast for UWP/js/main.js
+++ iMast for UWP/iMast for UWP/js/main.js
@@ -1,1 +1,5 @@
 ﻿// コードをここに挿入。
+WinJS.UI.processAll().done(function () {
+    var splitView = document.querySelector(".splitView").winControl;
+    new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
+});
