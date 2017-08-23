018b81c4503db20056a2002a7c084fe8274b800d
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2017-08-23 16:03:02 +0900

    大まかなUIの完成

--- iMast for UWP/iMast for UWP/js/main.js
+++ iMast for UWP/iMast for UWP/js/main.js
@@ -5,15 +5,16 @@ WinJS.UI.processAll().done(function () {
 });
 
 // Toolbar
-WinJS.Namespace.define("Sample", {
-    outputCommand: WinJS.UI.eventHandler(function (ev) {
-        var status = document.querySelector('.status');
-        var command = ev.currentTarget;
-        if (command.winControl) {
-            var label = command.winControl.label || command.winControl.icon || "button";
-            var section = command.winControl.section || "";
-            var msg = section + " command " + label + "was pressed.";
-            status.textContent = msg;
-        }
-    })
-});
+//WinJS.Namespace.define("ToolBar", {
+//    outputCommand: WinJS.UI.eventHandler(function (ev) {
+//        var status = document.querySelector('.status');
+//        var command = ev.currentTarget;
+//        if (command.winControl) {
+//            var label = command.winControl.label || command.winControl.icon || "button";
+//            var section = command.winControl.section || "";
+//            var msg = section + " command " + label + "was pressed.";
+//            status.textContent = msg;
+//        }
+//    })
+//});
+
