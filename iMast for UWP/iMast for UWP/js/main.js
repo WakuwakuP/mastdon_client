f01ffe0b4a2e665c9e1951e2e8154d0decf7e3c9
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2017-08-23 14:47:46 +0900

    Add ToolBar and POST画面

--- iMast for UWP/iMast for UWP/js/main.js
+++ iMast for UWP/iMast for UWP/js/main.js
@@ -3,3 +3,17 @@ WinJS.UI.processAll().done(function () {
     var splitView = document.querySelector(".splitView").winControl;
     new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
 });
+
+// Toolbar
+WinJS.Namespace.define("Sample", {
+    outputCommand: WinJS.UI.eventHandler(function (ev) {
+        var status = document.querySelector('.status');
+        var command = ev.currentTarget;
+        if (command.winControl) {
+            var label = command.winControl.label || command.winControl.icon || "button";
+            var section = command.winControl.section || "";
+            var msg = section + " command " + label + "was pressed.";
+            status.textContent = msg;
+        }
+    })
+});
