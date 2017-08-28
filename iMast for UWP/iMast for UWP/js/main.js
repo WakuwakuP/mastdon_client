256c80e54f6ec618cefccfab7f06bc6fa17af5f5
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2017-08-28 13:17:50 +0900

    app登録機能の追加

--- iMast for UWP/iMast for UWP/js/main.js
+++ iMast for UWP/iMast for UWP/js/main.js
@@ -24,12 +24,39 @@ WinJS.UI.processAll().done(function () {
 //    })
 //});
 
+// 初回起動時の処理
 var startUp = localSettings.values["StartUp"];
 
-if (!startUp || startUp == false) {
+if (!startUp || startUp === false) {
     localSettings.values["StartUp"] = false;
     var oauth = document.querySelector("#OAuth").winControl;
     oauth.show();
 }
-//else {
-//}
+
+function OAuth() {
+    var mastodonHost = $("#InstanceDomain").val();
+    var clientName = $("#clientName").val();
+    if (mastodonHost !== "" && clientName !== "") {
+        $.post("https://"+mastodonHost+"/api/v1/apps", {
+            client_name: clientName,
+            redirect_uris: "urn:ietf:wg:oauth:2.0:oob",
+            website: "https://wakuwakup.net/mastdon_client",
+            scopes: "read write follow"
+        }, function (json) {
+            localSettings.values["OAuth_id"] = json.id;
+            localSettings.values["OAuth_redirect_uri"] = json.redirect_uri;
+            localSettings.values["OAuth_client_id"] = json.client_id;
+            localSettings.values["OAuth_client_secret"] = json.client_secret;
+            document.querySelector("#OAuth").winControl.hide(none);
+            localSettings.values["StartUp"] = true;
+        });
+    }
+}
+
+(function () {
+    "use strict";
+    WinJS.Application.onready = function () {
+        WinJS.UI.processAll();
+    };
+    WinJS.Application.start();
+})();
